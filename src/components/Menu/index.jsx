ad970200b35c9040d8f51b0edc5242955c6a6e99
Alexandre De Carvalho Pimentel <apimentel@firjan.com.br>
2025-05-26 08:29:38 -0300

    first commit

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,15 @@
+import './styles.css'
+
+export const Menu = () => {
+    return (
+        <div className="menu-principal">
+            <h3>Menu</h3>
+            <ul>
+                <li>Home</li>
+                <li>Cadastro</li>
+                <li>Reservas</li>
+                <li>Contato</li>
+            </ul>
+        </div>
+    )
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import { Menu } from "../../components/Menu"
+import "./styles.css"
+
+function Home(){
+ return (
+    <div className="home">
+
+        <Menu />
+        
+        <div className="home-principal">
+
+        </div>
+        
+    </div>
+ )   
+}
+export default Home
